bc726e356ca4429169ca965373fa67758749d582
Kiyoon Ko <kiyoonko@gmail.com>
2017-01-13 06:17:50 -0500

    changed formatting of thank you page

--- app/src/main/java/com/example/michaelwang/capitaltogo/TransactionSuccessActivity.java
+++ app/src/main/java/com/example/michaelwang/capitaltogo/TransactionSuccessActivity.java
@@ -19,11 +19,9 @@ public class TransactionSuccessActivity extends AppCompatActivity {
         final Typeface custom_font_bold = Typeface.createFromAsset(getAssets(),  "fonts/Avenir-Heavy.ttf");
         final TextView thank = (TextView) findViewById(R.id.thank);
         final TextView account = (TextView) findViewById(R.id.account);
-        final TextView balance = (TextView) findViewById(R.id.balance);
         final ImageButton bAccount = (ImageButton) findViewById(R.id.bAccount);
         thank.setTypeface(custom_font_medium);
         account.setTypeface(custom_font_roman);
-        balance.setTypeface(custom_font_bold);
         //get balance and replace text in balance
         bAccount.setOnClickListener(new View.OnClickListener(){
             @Override
